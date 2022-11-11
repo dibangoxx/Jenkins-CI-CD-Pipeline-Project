37240111ee41f717c6c26e850e7750abc9be4292
dibango <dibangoxx@gmail.com>
2022-11-10 19:06:27 -0600

    commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,9 +16,6 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
-    string password = "Admin1825";
-    system.out.printIn("Hello");
-    
     return String.format("Hello Avinash, %s!", someone);
   }
 }
